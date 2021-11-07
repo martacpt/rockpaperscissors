84b3b3108d5949f166bfff2f4c40982ca04edc9c
martacpt <martajuscas@gmail.com>
2021-11-07 14:22:09 +0000

    some improvements (play again, score)

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const scissorsIcon = document.getElementById('scissors')
 
 const playerCurrScore = document.getElementById('playerscore')
 const gameDesc = document.getElementById('gamedesc')
+const playAgainBtn = document.getElementById('playagain')
 
 const computerCurrScore = document.getElementById('computerscore')
 
@@ -12,6 +13,8 @@ function computerPlay() {
     return options[Math.floor(Math.random()*options.length)]
 }
 
+
+// Function to get player selection via Prompt 
 /*function playerSelection() {
     let playerSelection = prompt("Choose Rock, Paper or Scissors")
     if (playerSelection.toLowerCase() != 'rock' && playerSelection.toLowerCase() != 'paper' && playerSelection.toLowerCase() != 'scissors') {
@@ -40,23 +43,26 @@ scissorsIcon.addEventListener('click', () => {
 })
  
 function game(playerChoice) {
-    let playNode = document.createElement('h3')
-    gameDesc.appendChild(playNode)
 
 
     if (!isGameOver) {
         let computerChoice = computerPlay()
+        let playNode = document.createElement('h3')
+        gameDesc.appendChild(playNode)
         if (computerChoice === playerChoice) {
             playerCurrScore.innerText = playerScore
             computerCurrScore.innerText = computerScore
             stats(playerChoice, computerChoice, playerScore, computerScore)
             gameDesc.innerHTML = `You both chose ${playerChoice}, it's a tie, play again`
+            checkEnd()
+
         } else if (computerChoice === 'rock' && playerChoice === 'paper') {
             playerScore += 1;
             playerCurrScore.innerText = playerScore
             computerCurrScore.innerText = computerScore
             stats(playerChoice, computerChoice, playerScore, computerScore)
             gameDesc.innerHTML = `You chose ${playerChoice}, Computer chose ${computerChoice}. You win!`
+            checkEnd()
 
         } else if (computerChoice === 'paper' && playerChoice === 'rock') {
             computerScore += 1;
@@ -64,6 +70,7 @@ function game(playerChoice) {
             computerCurrScore.innerText = computerScore
             stats(playerChoice, computerChoice, playerScore, computerScore)
             gameDesc.innerHTML = `You chose ${playerChoice}, Computer chose ${computerChoice}. You lose!`
+            checkEnd()
 
         } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
             computerScore += 1;
@@ -71,6 +78,7 @@ function game(playerChoice) {
             computerCurrScore.innerText = computerScore
             stats(playerChoice, computerChoice, playerScore, computerScore)
             gameDesc.innerHTML = `You chose ${playerChoice}, Computer chose ${computerChoice}. You lose!`
+            checkEnd()
 
         } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
             playerScore += 1;
@@ -78,6 +86,7 @@ function game(playerChoice) {
             computerCurrScore.innerText = computerScore
             stats(playerChoice, computerChoice, playerScore, computerScore)
             gameDesc.innerHTML = `You chose ${playerChoice}, Computer chose ${computerChoice}. You win!`
+            checkEnd()
 
         } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
             computerScore += 1;
@@ -85,6 +94,7 @@ function game(playerChoice) {
             computerCurrScore.innerText = computerScore
             stats(playerChoice, computerChoice, playerScore, computerScore)
             gameDesc.innerHTML = `You chose ${playerChoice}, Computer chose ${computerChoice}. You lose!`
+                        checkEnd()
 
         } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
             playerScore += 1;
@@ -92,19 +102,37 @@ function game(playerChoice) {
             computerCurrScore.innerText = computerScore
             stats(playerChoice, computerChoice, playerScore, computerScore)
             gameDesc.innerHTML = `You chose ${playerChoice}, Computer chose ${computerChoice}. You win!`
-
+            checkEnd()
         }
     
-        if (playerScore == 5 || computerScore == 5) {
-            isGameOver = true;
-            if (computerScore == 5) {
-                console.log('Computer Wins!')
-            } else {
-                console.log('You win!')
-            }
-        }
+
+   
     }
 }
+
+function checkEnd() {
+ if (playerScore == 5 || computerScore == 5) {
+     isGameOver = true;
+     playAgainBtn.classList.remove('hidden')
+    if (computerScore == 5) {
+        gameDesc.innerHTML = ('Computer Wins!')
+    } else {
+        gameDesc.innerHTML = ('Congratulation, you won!!')
+     }
+ }
+}
+
+playAgainBtn.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    isGameOver = false;
+    playAgainBtn.classList.add('hidden')
+    playerCurrScore.innerText = playerScore
+    computerCurrScore.innerText = computerScore
+    stats(playerChoice, computerChoice, playerScore, computerScore)
+    game()
+}
+)
 
 function stats(playerChoice, computerChoice, playerScore, computerScore) {
             console.log(`Your choice: ${playerChoice}`)
